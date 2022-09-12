a6d9c8519bc861f3e62d5b964aa956e47e2378dc
Aurelien Carray <aurelien.carray@orange.fr>
2022-09-12 10:52:28 +0100

    api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,57 @@
 import { Component } from '@angular/core';
-import {Router} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+@Injectable()
 export class AppComponent {
-  title = 'To Do List';
+  title = 'Pari-Ligue 1';
+  url: string = 'https://www.wellingtonsoccer.com/lib/api/auth.cfc?returnFormat=JSON&method=Authenticate';
 
-  constructor(private router: Router) {}
+  constructor(private http: HttpClient) {}
 
   public connect(): boolean {
     return sessionStorage.getItem('isConnected') == 'true';
   }
+
+  /*public getInfos(): void {
+    var require: any;
+    var request = require('request');
+    var options = {
+      'method': 'GET',
+      'url': 'https://v1.baseball.api-sports.io/{endpoint}',
+      'headers': {
+        'x-rapidapi-key': '395f56620e52841c3d7683b92e2ebf14',
+        'x-rapidapi-host': 'v1.baseball.api-sports.io'
+      }
+    };
+    request(options, function (error: string | undefined, response: { body: any; }) {
+      if (error) throw new Error(error);
+      console.log(response.body);
+      }); 
+    }
+
+    sendPostRequest() {
+      const headers = new HttpHeaders()
+        .set('cache-control', 'no-cache')
+        .set('content-type', 'application/json')
+        .set('postman-token', '395f56620e52841c3d7683b92e2ebf14');
+
+        const body = {
+          email: 'myemail@xyz.com',
+          user_password: 'mypasss',
+          token: 'my token'
+        }
+
+        this.http.get<any>('https://api.npms.io/v2/search?q=scope:angular').subscribe(data => {
+          this.totalAngularPackages = data.total;
+        })
+
+        return this.http.post(this.url, body, { headers: headers }).subscribe(response => console.log(response));
+    }   */    
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -8,29 +7,16 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { HttpClientModule } from '@angular/common/http';
-
 import { ReactiveFormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field';
-
 import { FormsModule } from '@angular/forms';
-
-
-
 import { AuthService } from './core/services/auth.service';
-
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
 import { AddTaskComponent } from './pages/taches/add-task/add-task.component';
-
 import { CoreModule } from './core/core.module';
-
-
-
-
 
 @NgModule({
   declarations: [
