bfa4089af6f9a6ef621a08fa32d48a52b47c72d2
GuerHat <75025343+EngueranR@users.noreply.github.com>
2022-11-01 21:49:28 +0100

    API + JSON + PAGE MATCH

--- src/app/pages/match/match.component.ts
+++ src/app/pages/match/match.component.ts
@@ -1,14 +1,19 @@
 import {Component, OnInit} from '@angular/core';
-import matchJson from '../../../assets/data/match.json';
+// @ts-ignore
+import matchJson from '../../../assets/data/ligue1_API.json';
+// @ts-ignore
+import colorTeam from '../../../assets/data/teamColor.json';
 
 
 interface Imatch {
+  day: string;
   idMatch: string;
-  equipeDomicile: string;
-  equipeExterieur: string;
-  scoreDomicile: string;
-  scoreExterieur: string;
-  journee: string;
+  homeTeam: string;
+  awayTeam: string;
+  scoreHomeTeam: string;
+  scoreAwayTeam: string;
+  colorHomeTeam: string;
+  colorAwayTeam: string;
 }
 
 
@@ -20,14 +25,76 @@ interface Imatch {
 export class MatchComponent implements OnInit {
   constructor() {
   }
-
-  matchs: Imatch[] = matchJson;
-
-
+  // initalise le json ligue1_API
+  ligue1_API: Imatch[] = matchJson;
+  // initalise la couleur pour pouvoir modifié les couleurs par rapport aux equipes
+  color: string;
+  matchs: any;
 
 
   ngOnInit(): void {
+
+    this.color = "";
   }
+
+
+  public colorHomeTeamOnMouse(teamSelect: string) {
+    for (let match of this.ligue1_API) {
+      // Si le json est égale a la team afficher dans la classe team
+      if (match.homeTeam === teamSelect) {
+        this.color = '#' + match.colorHomeTeam;
+      }
+    }
+  }
+
+  public colorAwayTeamOnMouse(teamSelect: string) {
+    for (let match of this.ligue1_API) {
+      // Si le json est égale a la team afficher dans la classe team
+      if (match.awayTeam === teamSelect) {
+        this.color = '#' + match.colorAwayTeam;
+      }
+    }
+  }
+
+  /*
+
+  public findColorTeam() {
+    // recupere le contenu de la classe team
+    const teamHTML = document.querySelector<HTMLElement>('.selectTeam')!;
+    // colorTeam est le json contenant les noms d'equipe et les couleurs
+    // on parcourt le json
+    for (let tcolor of colorTeam) {
+      // Si le json est égale a la team afficher dans la classe team
+      if (tcolor.team === teamHTML.innerText) {
+        // on change la couleur
+        teamHTML.style.color = '#' + tcolor.hex;
+      }
+    }
+    teamHTML.classList.remove('selectTeam');
+    teamHTML.classList.add('resetColor');
+
+  }
+
+
+  colorResetTeam(teamName: Imatch) {
+    const teamHTML = document.querySelector<HTMLElement>('.resetColor')!;
+    teamHTML.style.color = '#3646a8';
+    teamHTML.classList.remove('resetColor');
+  }
+
+  public colorTeamHome(teamName: Imatch) {
+    this.selectedTeam = teamName.homeTeam;
+    this.findColorTeam();
+
+  }
+
+  public colorTeamAway(teamName: Imatch) {
+    this.selectedTeam = teamName.awayTeam;
+    this.findColorTeam();
+  }
+
+
+*/
 
 
 }
