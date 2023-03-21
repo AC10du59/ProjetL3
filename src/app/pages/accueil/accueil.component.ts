e1860e55c284433545d509f3fcc13f78a97cc293
Estebann Koenig <estebannemmy@gmail.com>
2023-03-21 13:23:49 +0100

    accueil

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent implements OnInit {
 
-  constructor() { }
+export class AccueilComponent {
+  users: any[] = [];
+  communaute: any[];
 
-  ngOnInit(): void {
+  constructor(private firestore: AngularFirestore) {}
+
+  ngOnInit() {
+    this.firestore.collection('users').valueChanges().subscribe((users: any[]) => {
+      this.users = users;
+    });
+  
+      this.firestore.collection('communaute').valueChanges().subscribe((communaute: any[]) => {
+        this.communaute = communaute;
+    });
   }
 
+  getNumberOfUsers(): number {
+    return this.users.length;
+  }
+
+
+  getNumberOfCommunaute(): number {
+    return this.communaute.length;
+  }
 }
+
